3a79a609e84a48e09019dd1a9a22773b446e6a00
ShinSub98 <wjdtjq26@naver.com>
2024-11-17 05:18:35 +0900

    <feat>: disabled csrf protection for local environment

--- src/main/java/site/talent_trade/api/config/SecurityConfig.java
+++ src/main/java/site/talent_trade/api/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package site.talent_trade.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,4 +16,14 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
+  // 로컬 환경에서만 csrf 보호 비활성화
+  @Bean
+  @Profile("local")
+  protected SecurityFilterChain localCsrfConfigure(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/**").permitAll()
+        );
+    return http.build();
+  }
 }
