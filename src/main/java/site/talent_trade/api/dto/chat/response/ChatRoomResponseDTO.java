d3f5ab0166f037286cb8a9c947c903770cb9ff78
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-11-17 22:05:45 +0900

    <refact> fix DTO

--- src/main/java/site/talent_trade/api/dto/chat/response/ChatRoomResponseDTO.java
+++ src/main/java/site/talent_trade/api/dto/chat/response/ChatRoomResponseDTO.java
@@ -1,22 +1,24 @@
 package site.talent_trade.api.dto.chat.response;
 
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import site.talent_trade.api.domain.member.Talent;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @Builder
 public class ChatRoomResponseDTO {
 
+    private Long roomId;
     private String opponentNickname; //상대방 닉네임
     private Talent talent; //재능
     private String detailTalent; //세부재능
     private String lastMessage; //마지막 메시지
     private LocalDateTime lastMessageAt; //마지막 메시지를 받은 시간
 
-    public ChatRoomResponseDTO(String opponentNickname, Talent talent, String detailTalent, String lastMessage, LocalDateTime lastMessageAt) {
+    public ChatRoomResponseDTO(Long roomId, String opponentNickname, Talent talent, String detailTalent, String lastMessage, LocalDateTime lastMessageAt) {
+        this.roomId = roomId;
         this.opponentNickname = opponentNickname;
         this.talent = talent;
         this.detailTalent = detailTalent;
