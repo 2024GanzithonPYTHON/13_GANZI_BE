2c1bead5871999a3dc53dff3d99013f1027c5c48
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-11-21 01:18:22 +0900

    <fix> fix url path and add chatRoomId/complete

--- src/main/java/site/talent_trade/api/controller/chat/ChatRoomController.java
+++ src/main/java/site/talent_trade/api/controller/chat/ChatRoomController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import site.talent_trade.api.domain.chat.ChatRoom;
+import site.talent_trade.api.dto.chat.response.ChatRoomCompletedDTO;
 import site.talent_trade.api.dto.chat.response.ChatRoomResponseDTO;
 import site.talent_trade.api.service.chat.ChatRoomService;
 import site.talent_trade.api.util.jwt.JwtProvider;
@@ -17,7 +18,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/chat")
+@RequestMapping("/chatrooms")
 public class ChatRoomController {
     //내가 참여한 채팅방 리스트 조회
     //반환해야하는 값 : 상대방의 닉네임, 분야, 세부분야, 마지막 메시지, 마지막 메시지를 보낸 시간
@@ -29,18 +30,23 @@ public class ChatRoomController {
     private JwtProvider jwtProvider;
 
     //채팅방 생성
-    @PostMapping("/createChatRoom")
+    @PostMapping
     public ResponseDTO<ChatRoomResponseDTO> createChatRoom(HttpServletRequest request, @RequestParam Long toMemberId) {
         Long fromMemberId = jwtProvider.validateToken(request);
         return chatRoomService.createChatRoom(fromMemberId, toMemberId);
     }
 
     // 내가 참여한 채팅방 조회
-    @GetMapping("/getChatRoomList")
+    @GetMapping
     public ResponseDTO<List<ChatRoomResponseDTO>> getMyChatRooms(HttpServletRequest request) {
         Long memberId = jwtProvider.validateToken(request);
         return chatRoomService.getChatRoomList(memberId);
     }
 
-
+    //만남 성사하기
+    @PatchMapping("/{chatRoomId}/complete")
+    public ResponseDTO<ChatRoomCompletedDTO> completeChatRoom(HttpServletRequest request, @PathVariable Long chatRoomId) {
+        jwtProvider.validateToken(request);
+        return chatRoomService.completeChatRoom(chatRoomId);
+    }
 }
